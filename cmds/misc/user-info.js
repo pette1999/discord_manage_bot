b79e52ac44d1ed9a49590da3b3ea914b60a0b2e2
pette1999 <haichen@chapman.edu>
2022-04-16 07:17:20 -0700

    update

---
+++ cmds/misc/user-info.js
@@ -0,0 +1,80 @@
+const { MessageEmbed } = require('discord.js')
+const Commando = require('discord.js-commando')
+let statbot_message = require('../../statbot/data/message.json');
+let statbot_voice = require('../../statbot/data/voice.json')
+
+module.exports = class UserInfoCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'userinfo',
+            group: 'misc',
+            memberName: 'userinfo',
+            description: 'Displays information a user',
+        })
+    }
+
+    run = async(message) => {
+        const { guild, channel } = message
+        const inviteCounter = {}
+        const messageCounter = {}
+        const voiceCounter = {}
+
+        for (const m of statbot_message) {
+            messageCounter[m['membertag']] = m['count']
+        }
+        for (const m of statbot_voice) {
+            voiceCounter[m['membertag']] = m['count']
+        }
+
+        guild.fetchInvites().then((invites) => {
+            invites.forEach((invite) => {
+                const { uses, inviter } = invite
+                const { username, discriminator } = inviter
+
+                const name = `${username}#${discriminator}`
+
+                inviteCounter[name] = (inviteCounter[name] || 0) + uses
+            })
+
+            console.log(inviteCounter)
+            const user = message.mentions.users.first() || message.member.user
+            const member = guild.members.cache.get(user.id)
+
+            console.log(member)
+            console.log(inviteCounter[user.tag])
+
+            const embed = new MessageEmbed()
+                .setAuthor(`User info for ${user.username}`, user.displayAvatarURL())
+                .addFields({
+                    name: 'User tag',
+                    value: user.tag,
+                }, {
+                    name: 'Is bot',
+                    value: user.bot,
+                }, {
+                    name: 'Nickname',
+                    value: member.nickname || 'None',
+                }, {
+                    name: 'Joined Server',
+                    value: new Date(member.joinedTimestamp).toLocaleDateString(),
+                }, {
+                    name: 'Joined Discord',
+                    value: new Date(user.createdTimestamp).toLocaleDateString(),
+                }, {
+                    name: 'Roles',
+                    value: member.roles.cache.size - 1,
+                }, {
+                    name: 'invites',
+                    value: inviteCounter[user.tag],
+                }, {
+                    name: 'Message Count',
+                    value: messageCounter[user.tag],
+                }, {
+                    name: 'Voice Count',
+                    value: voiceCounter[user.tag],
+                })
+
+            channel.send(embed)
+        })
+    }
+}
---
+++ statbot/scrape.py
@@ -0,0 +1,59 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+import json
+import csv
+import os
+
+def removeJsonFile(filePath):
+    if os.path.exists(filePath):
+        os.remove(filePath)
+    else:
+        print("Can not delete the file as it doesn't exists")
+
+
+voice_url = "https://proxy.statbot.net/servervoice/948732804999553034?user_id=921366394631761994"
+message_url = "https://proxy.statbot.net/servermessage/948732804999553034?user_id=921366394631761994"
+
+headers = CaseInsensitiveDict()
+headers["Host"] = "proxy.statbot.net"
+headers["accept"] = "application/json"
+headers["cache-control"] = "no-cache"
+headers["user-agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
+headers["token"] = "Yj^dUX2M]Ur~'9?pQI-xD0>`Lu]#60Qs}~K9S8cF<6uq#t{`+P${"
+headers["sec-gpc"] = "1"
+headers["origin"] = "https://statbot.net"
+headers["sec-fetch-site"] = "same-site"
+headers["sec-fetch-mode"] = "cors"
+headers["sec-fetch-dest"] = "empty"
+headers["referer"] = "https://statbot.net/"
+headers["accept-language"] = "en-US,en;q=0.9"
+
+def grabVoice():
+  voice = []
+  try:
+    voice_resp = requests.get(voice_url, headers=headers)
+    for i in voice_resp.json().get("topMembers"):
+      voice.append(i)
+  except:
+    pass
+  removeJsonFile('./data/voice.json')
+  with open('./data/voice.json', 'w', encoding='utf-8') as f:
+    json.dump(voice, f, ensure_ascii=False, indent=2)
+
+def grabMessage():
+  message = []
+  try:
+    message_resp = requests.get(message_url, headers=headers)
+    for i in message_resp.json().get("topMembers"):
+      message.append(i)
+  except:
+    pass
+  removeJsonFile('./data/message.json')
+  with open('./data/message.json', 'w', encoding='utf-8') as f:
+    json.dump(message, f, ensure_ascii=False, indent=2)
+
+def main():
+  grabVoice()
+  grabMessage()
+
+main()
