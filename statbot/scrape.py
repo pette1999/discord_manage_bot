748de4a78e525bdfb4a0aa0a3bf13ff20e372af6
pette1999 <haichen@chapman.edu>
2022-04-25 16:01:46 -0700

    update scoring

--- statbot/scrape.py
+++ statbot/scrape.py
@@ -58,7 +58,7 @@ def grabMessage():
 
 def getTime():
   now = datetime.now()
-  dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+  dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
   return (dt_string)
   
 def main():
